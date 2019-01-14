00a6543c02c49cd046ae96000e8c0118cdbe0669
Steve Robertson <SteveRiosapps@gmail.com>
2019-01-14 09:52:19 -0600

    Initial Commit

---
+++ ProductionControlTaskTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductionControlTaskTracking
+//
+//  Created by Steven Robertson on 1/14/19.
+//  Copyright © 2019 Steven Robertson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
